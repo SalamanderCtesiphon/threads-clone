13cba102147685d386167d5d8c7de958426c14f2
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-28 17:25:47 -0500

    reformatted the code

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Bottombar from "@/components/shared/Bottombar";
