e144f021485d0bcc7e94b06d4c0d7a5800536af7
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-28 16:44:36 -0500

    imported some constants

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function leftSidebar() {
   return (
-    <div>leftSidebar</div>
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+        
+      </div>
+    </section>
   )
 }
 
