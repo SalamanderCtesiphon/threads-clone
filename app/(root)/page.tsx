955e33d0313c93dc24d71a19687c384f90550fbe
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-28 15:11:44 -0500

    completed the set-up for the clerk app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Threads</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   )
 }
