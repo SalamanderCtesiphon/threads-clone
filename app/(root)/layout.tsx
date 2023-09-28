ddbac78b34179eb69f1762f2aaf3035573d4a128
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-28 15:27:52 -0500

    created components for the main app layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,9 @@
 import '../globals.css'
 import { Inter } from 'next/font/google'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +23,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
