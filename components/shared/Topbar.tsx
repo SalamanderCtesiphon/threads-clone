27be2289997528be9c378903dd33d832d5fd95f1
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-28 15:44:42 -0500

    added svgs

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 function Topbar() {
   return (
-    <div>Topbar</div>
+    <nav className='topbar'>
+      <Link href={'/'} className='flex items-center gap-4'>
+        <Image 
+          src="/logo.svg"
+          alt="logo"
+          width={28}
+          height={28}
+        />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+      </Link>
+    </nav>
   )
 }
 
