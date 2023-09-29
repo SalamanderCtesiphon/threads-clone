818b9fca18528223315aeffaac0dcf7c97bf0f59
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-28 21:37:54 -0500

    implemented the ability to change user's profile pics

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import { UserValidation } from '@/lib/validations/user';
@@ -32,6 +32,7 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle}: Props) => {
+  const [files, setFiles] = useState<File[]>([])
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -43,15 +44,54 @@ const AccountProfile = ({ user, btnTitle}: Props) => {
     }
   })
 
-  const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
+
+    const hasImageChanged = isBase64Image(blob);
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
+
+    await updateUser({
+      name: values.name,
+      path: pathname,
+      username: values.username,
+      userId: user.id,
+      bio: values.bio,
+      image: values.profile_photo,
+    });
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
+
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
     e.preventDefault();
 
-  }
+    const fileReader = new FileReader();
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    if(e.target.files && e.target.files.length> 0) {
+      const file = e.target.files[0];
+
+      setFiles(Array.from(e.target.files));
+
+      if(!file.type.includes('image')) return
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+
+        fieldChange(imageDataUrl)
+      }
+
+      fileReader.readAsDataURL(file)
+    }
   }
 
   return (
