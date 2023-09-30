556c10377eb478fc6221dac5e7fb51809ffc5b16
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-30 14:03:37 -0500

    created the comment form

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-//import Comment from "@/components/forms/Comment";
+import Comment from "@/components/forms/Comment";
 import ThreadCard from "@/components/cards/ThreadCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
