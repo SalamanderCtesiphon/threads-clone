d1b8847a8f6986535c3da365b6e99676d25c9abf
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-30 15:05:22 -0500

    updated the onboarding

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -16,7 +16,7 @@ async function Page({ params }: { params: { id: string } }) {
 
   const userInfo = await fetchUser(params.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
-
+  console.log(user)
   return (
     <section>
       <ProfileHeader
