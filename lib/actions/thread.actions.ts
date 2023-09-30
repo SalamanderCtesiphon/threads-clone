3d02a12006b2f55afa50e79897458f08e416b6d9
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-29 19:15:45 -0500

    set up the thread schema

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,3 +1,21 @@
-export async function createThread() {
+import { connectToDB } from "../mongoose"
+import { ThreadValidation } from "../validations/thread"
+
+interface Params {
+  text: string, 
+  author: string, 
+  communityId: string | null,
+  path: string,
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path
+}: Params) {
+  connectToDB()
+
+  const createdThread = await Thread.create()
   return
 }
