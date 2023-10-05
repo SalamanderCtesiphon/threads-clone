0c4f3ced4a90f2145809ced19c3ef228f8d8f196
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-04 21:04:12 -0500

    reverted back to a previous build

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,7 +4,7 @@
 
 // Resource: https://docs.svix.com/receiving/verifying-payloads/why
 // It's a good practice to verify webhooks. Above article shows why we should do it
-/* import { Webhook, WebhookRequiredHeaders } from "svix";
+import { Webhook, WebhookRequiredHeaders } from "svix";
 import { headers } from "next/headers";
 
 import { IncomingHttpHeaders } from "http";
@@ -35,42 +35,5 @@ export const POST = async (request: Request) => {
   } catch (err) {
     return NextResponse.json({ message: err }, { status: 400 });
   }
-} */
-
-import type { IncomingHttpHeaders } from 'http';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { WebhookRequiredHeaders } from 'svix';
-import type { WebhookEvent } from '@clerk/nextjs/server';
-import { Webhook } from 'svix';
- 
-const webhookSecret = process.env.WEBHOOK_SECRET;
- 
-export default async function handler(
-  req: NextApiRequestWithSvixRequiredHeaders,
-  res: NextApiResponse
-) {
-  const payload = JSON.stringify(req.body);
-  const headers = req.headers;
-  // Create a new Webhook instance with your webhook secret
-  const wh = new Webhook(webhookSecret || '');
- 
-  let evt: WebhookEvent;
-  try {
-    // Verify the webhook payload and headers
-    evt = wh.verify(payload, headers) as WebhookEvent;
-  } catch (_) {
-    // If the verification fails, return a 400 error
-    return res.status(400).json({});
-  }
-  const { id } = evt.data;
- 
-  const eventType = evt.type;
-  if (eventType === 'user.created') {
-    console.log(`User ${id} was ${eventType}`);
-    res.status(201).json({});
-  }
 }
- 
-type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-};
+
