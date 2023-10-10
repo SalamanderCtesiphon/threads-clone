fcba1d5dd92e88ec18e80733cf77b80212b8d3c0
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-10 16:53:29 -0500

    added another loading page

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useState, CSSProperties } from "react";
+import PacmanLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "2 auto",
+  borderColor: "#7878a3",
+};
+
+export default function Loading() {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#7878a3");
+  // You can add any UI inside Loading, including a Skeleton.
+  return       <PacmanLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={25}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,6 @@ import Pagination from "@/components/shared/Pagination";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 
-import { Suspense } from "react";
-
 async function Home({
   searchParams,
 }: {
@@ -30,26 +28,24 @@ async function Home({
       <h1 className='head-text text-left'>Home</h1>
 
       <section className='mt-9 flex flex-col gap-10'>
-        <Suspense fallback={<p>Page Loading... </p>}>
-          {result.posts.length === 0 ? (
-            <p className='no-result'>No threads found</p>
-          ) : (
-            <>
-              {result.posts.map((post) => (
-                <ThreadCard
-                  key={post._id}
-                  id={post._id}
-                  currentUserId={user.id}
-                  parentId={post.parentId}
-                  content={post.text}
-                  author={post.author}
-                  createdAt={post.createdAt}
-                  comments={post.children}
-                />
-              ))}
-            </>
-          )}
-        </Suspense>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
       </section>
 
       <Pagination
