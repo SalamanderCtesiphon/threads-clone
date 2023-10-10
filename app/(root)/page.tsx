4ac18110afe36be624580c47f1a46ea1c58d29de
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-10 16:09:59 -0500

    used react suspense

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,8 @@ import Pagination from "@/components/shared/Pagination";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 
+import { Suspense } from "react";
+
 async function Home({
   searchParams,
 }: {
@@ -28,24 +30,26 @@ async function Home({
       <h1 className='head-text text-left'>Home</h1>
 
       <section className='mt-9 flex flex-col gap-10'>
-        {result.posts.length === 0 ? (
-          <p className='no-result'>No threads found</p>
-        ) : (
-          <>
-            {result.posts.map((post) => (
-              <ThreadCard
-                key={post._id}
-                id={post._id}
-                currentUserId={user.id}
-                parentId={post.parentId}
-                content={post.text}
-                author={post.author}
-                createdAt={post.createdAt}
-                comments={post.children}
-              />
-            ))}
-          </>
-        )}
+        <Suspense fallback={<p>Page Loading... </p>}>
+          {result.posts.length === 0 ? (
+            <p className='no-result'>No threads found</p>
+          ) : (
+            <>
+              {result.posts.map((post) => (
+                <ThreadCard
+                  key={post._id}
+                  id={post._id}
+                  currentUserId={user.id}
+                  parentId={post.parentId}
+                  content={post.text}
+                  author={post.author}
+                  createdAt={post.createdAt}
+                  comments={post.children}
+                />
+              ))}
+            </>
+          )}
+        </Suspense>
       </section>
 
       <Pagination
