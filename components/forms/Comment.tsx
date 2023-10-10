261e622f28d69b59e21d09c1f448e701318ec7ac
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-10 16:33:03 -0500

    cleaned up the layout of the code

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { usePathname } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 import {
   Form,
   FormControl,
@@ -13,10 +12,8 @@ import {
   FormItem,
   FormLabel,
 } from "@/components/ui/form";
-
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-
 import { CommentValidation } from "@/lib/validations/thread";
 import { addCommentToThread } from "@/lib/actions/thread.actions";
 
